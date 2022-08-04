78f009907e9d9c10b4bf02801c5226dc40da2049
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-04 01:10:58 +0100

    Feat(app/urls): created app/urls.py

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from app.views import index, about
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

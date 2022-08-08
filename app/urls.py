eedab65c965b4472f10d0c0b2facc915c8fb8bfa
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-08 16:46:24 +0100

    Feat(app/urls): added url paths for the updated views to the urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from app.views import index, about
+from app.views import index, about, contact, faq, reviews
 
 urlpatterns = [
   path('', index, name='index'),
   path('about', about, name='about'),
+  path('contact', contact, name='contact'),
+  path('faq', faq, name='faq'),
+  path('reviews', reviews, name='reviews'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

114d5321d017f543d78d3dff66acf9d3cc493a21
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-08 16:45:09 +0100

    Feat(app/views): added contact, faq & reviews view functions

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,11 +7,48 @@ from django.shortcuts import render
 def index(request):
   template_name = 'index.html'
 
-  return render(request, template_name)
+  context = {
+    'current': 'index'
+  }
+
+  return render(request, template_name, context)
 
 # About view
 def about(request):
   template_name = 'about.html'
 
-  return render(request, template_name)
+  context = {
+    'current': 'about'
+  }
 
+  return render(request, template_name, context)
+
+# Contact view
+def contact(request):
+  template_name = 'contact.html'
+
+  context = {
+    'current': 'contact'
+  }
+
+  return render(request, template_name, context)
+
+# Faq view
+def faq(request):
+  template_name = 'faq.html'
+
+  context = {
+    'current': 'faq'
+  }
+
+  return render(request, template_name, context)
+
+# Reviews view
+def reviews(request):
+  template_name = 'review.html'
+
+  context = {
+    'current': 'reviews'
+  }
+
+  return render(request, template_name, context)

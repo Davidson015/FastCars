0da4c5960dec47de462d42da451cd79ecbae570d
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-10 00:03:31 +0100

    Fix(account/urls): corrected the import statement

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from app.views import account
+from account.views import account
 
 urlpatterns = [
   path('account', account, name='account'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

fd78769e22fbeee1d7842339a8d5409f9b7e4f38
Zeerex23 <tobilobadiekola@gmail.com>
2022-08-09 18:22:49 +0100

    Feat(account/views): created account view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Account view
+def account(request):
+  template_name = 'account.html'
+
+  context = {
+    'current': 'account'
+  }
+
+  return render(request, template_name, context)
